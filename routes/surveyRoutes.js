e4bed10c56965f8347e256cbb9884e7c05b2db61
“ani.aleksandraaa@gmail.com” <ani.aleksandraaa@gmail.com>
2019-07-21 23:55:15 +0200

    Final commit

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,15 @@ const { Path }= require('path-parser');
 const { URL } = require('url');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find( { _user: req.user.id } ).select({
+            recipients: false
+        });
+
+        res.send(surveys);
+    });
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
@@ -40,15 +48,33 @@ module.exports = app => {
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        console.log(req);
-        const events = _.map(req.body, ({email, url}) => {
-            const pathname = new URL(url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
-            if(match) {
-                return { email, surveyId: match.surveyId, choice: match.choice };
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        _.chain(req.body)
+          .map(({ email, url }) => {
+            const match = p.test(new URL(url).pathname);
+            if (match) {
+              return { email, surveyId: match.surveyId, choice: match.choice };
             }
-        });
-        console.log(events);
-    });
+          })
+          .compact()
+          .uniqBy('email', 'surveyId')
+          .each(({ surveyId, email, choice }) => {
+            Survey.updateOne(
+              {
+                _id: surveyId,
+                recipients: {
+                  $elemMatch: { email: email, responded: false }
+                }
+              },
+              {
+                $inc: { [choice]: 1 },
+                $set: { 'recipients.$.responded': true },
+                lastResponded: new Date()
+              }
+            ).exec();
+          })
+          .value();
+    
+        res.send({});
+      });
 };
